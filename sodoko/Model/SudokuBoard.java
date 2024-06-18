1b7c8d008ef1a06cb64406c32604e3b6b0f603cb
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2024-06-17 20:33:29 -0700

    Add Attribute SudokuBoard

---
+++ sodoko/Model/SudokuBoard.java
@@ -0,0 +1,180 @@
+public class SudokuBoard {
+
+   private ArrayList<ArrayList<Integer>> numberPool;
+    private boolean[][] forbiddenMove;
+    private Random rand;
+    private int[][] board;
+    private int[][] originalBoard;
+    public static int outpot[][];
+
+    @FXML
+    private TextField text26 ;
+    @FXML
+    private TextField text69;
+    @FXML
+    private TextField text25;
+    @FXML
+    private TextField text28;
+    @FXML
+    private TextField text27;
+    @FXML
+    private TextField text29;
+    @FXML
+    private TextField text62;
+    @FXML
+    private TextField text61;
+    @FXML
+    private TextField text64;
+    @FXML
+    private TextField text63;
+    @FXML
+    private TextField text66;
+    @FXML
+    private TextField text22;
+    @FXML
+    private TextField text65;
+    @FXML
+    private TextField text21;
+    @FXML
+    private TextField text68;
+    @FXML
+    private TextField text24;
+    @FXML
+    private TextField text67;
+    @FXML
+    private TextField text23;
+    @FXML
+    private TextField text59;
+    @FXML
+    private TextField text15;
+    @FXML
+    private TextField text58;
+    @FXML
+    private TextField text14;
+    @FXML
+    private TextField text17;
+    @FXML
+    private TextField text16;
+    @FXML
+    private TextField text19;
+    @FXML
+    private TextField text18;
+    @FXML
+    private TextField text95;
+    @FXML
+    private TextField text51;
+    @FXML
+    private TextField text94;
+    @FXML
+    private TextField text97;
+    @FXML
+    private TextField text53;
+    @FXML
+    private TextField text96;
+    @FXML
+    private TextField text52;
+    @FXML
+    private TextField text99;
+    @FXML
+    private TextField text55;
+    @FXML
+    private TextField text11;
+    @FXML
+    private TextField text98;
+    @FXML
+    private TextField text54;
+    @FXML
+    private TextField text57;
+    @FXML
+    private TextField text13;
+    @FXML
+    private TextField text56;
+    @FXML
+    private TextField text12;
+    @FXML
+    private TextField text91;
+    @FXML
+    private TextField text93;
+    @FXML
+    private TextField text92;
+    @FXML
+    private TextField text48;
+    @FXML
+    private TextField text47;
+    @FXML
+    private TextField text49;
+    @FXML
+    private TextField text84;
+    @FXML
+    private TextField text83;
+    @FXML
+    private TextField text86;
+    @FXML
+    private TextField text42;
+    @FXML
+    private TextField text85;
+    @FXML
+    private TextField text41;
+    @FXML
+    private TextField text88;
+    @FXML
+    private TextField text44;
+    @FXML
+    private TextField text87;
+    @FXML
+    private TextField text43;
+    @FXML
+    private TextField text46;
+    @FXML
+    private TextField text89;
+    @FXML
+    private TextField text45;
+    @FXML
+    private TextField text82;
+    @FXML
+    private TextField text81;
+    @FXML
+    private TextField text37;
+    @FXML
+    private TextField text36;
+    @FXML
+    private TextField text39;
+    @FXML
+    private TextField text38;
+    @FXML
+    private TextField text73;
+    @FXML
+    private TextField text72;
+    @FXML
+    private TextField text75;
+    @FXML
+    private TextField text31;
+    @FXML
+    private TextField text74;
+    @FXML
+    private TextField text77;
+    @FXML
+    private TextField text33;
+    @FXML
+    private TextField text76;
+    @FXML
+    private TextField text32;
+    @FXML
+    private TextField text79;
+    @FXML
+    private TextField text35;
+    @FXML
+    private TextField text78;
+    @FXML
+    private TextField text34;
+    @FXML
+    private TextField text71;
+    @FXML
+    private Button buttenStart;
+
+
+
+
+
+
+}
