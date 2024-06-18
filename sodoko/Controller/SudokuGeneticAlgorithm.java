4d9ec5f0e605072a58c83757959f9376327c46f9
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2024-06-17 20:26:01 -0700

    Add function controller

--- sodoko/Controller/SudokuGeneticAlgorithm.java
+++ sodoko/Controller/SudokuGeneticAlgorithm.java
@@ -19,7 +19,42 @@ public class SudokuGeneticAlgorithm{
         generatePopulation(sb, basePopulation);
     }
 
+ public void run(int generations){
+        int count = 0;
+
+        scoreFirstGeneration();
+
+        while(count < generations && bestScore < bestGenetics){
+            
+            selection();
 
 
+            for(int i = 0; i < matingSelections.size(); i+=2){
+                crossOver(matingSelections.get(i), matingSelections.get(i+1));
+            }
+            ++count;
+        }
+    }
+ public void selection(){
+        matingSelections.clear(); 
+        sortPopulation();
+
+        int limit = (population.size() > 100) ? 100 : population.size();
+
+        for(int i = 0; i < limit; i++){
+            matingSelections.add(population.get(i));
+        }
+
+        
+        if(matingSelections.size() % 2 != 0){
+            matingSelections.add(matingSelections.get(rand.nextInt(matingSelections.size()-1)));
+        }
+
+        
+        while(population.size() > 100){
+            population.remove(population.size()-1);
+            populationScore.remove(population.size()-1);
+        }
+    }
 
 }
