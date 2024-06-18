9d312a6c2028e783fe4c18cfe9fd8578147e8f4b
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2024-06-17 20:18:35 -0700

    editMain

--- sodoko/main.java
+++ sodoko/main.java
@@ -1,23 +1,20 @@
 
-import com.sun.org.apache.bcel.internal.classfile.Node;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/manu.fxml"));
-        loader.load();
-        primaryStage.setScene(new Scene(loader.getRoot()));
+        FXMLLoader fxmlLoader=new FXMLLoader(this.getClass().getResource("/view/manu.fxml"));
+        fxmlLoader.load();
+        primaryStage.setScene(new Scene(fxmlLoader.getRoot()));
+        primaryStage.setResizable(false);//این برای اینکه صفحه بزرگ نشه
         primaryStage.show();
-
     }
 }
