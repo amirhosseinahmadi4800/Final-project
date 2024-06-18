343291d75c5efa04bd4d6bb4c2a56dc26be4b9fe
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2024-06-17 20:42:17 -0700

    Add function input

--- sodoko/Model/SudokuBoard.java
+++ sodoko/Model/SudokuBoard.java
@@ -172,7 +172,37 @@ public class SudokuBoard {
     @FXML
     private Button buttenStart;
 
+private void defaultBoard() {
+        board = new int[][]{
+                {Integer.parseInt(text11.getText()), Integer.parseInt(text12.getText()), Integer.parseInt(text13.getText()), Integer.parseInt(text14.getText()), Integer.parseInt(text15.getText()), Integer.parseInt(text16.getText()), Integer.parseInt(text17.getText()), Integer.parseInt(text18.getText()), Integer.parseInt(text19.getText())},
+                {Integer.parseInt(text21.getText()), Integer.parseInt(text22.getText()), Integer.parseInt(text23.getText()), Integer.parseInt(text24.getText()), Integer.parseInt(text25.getText()), Integer.parseInt(text26.getText()), Integer.parseInt(text27.getText()), Integer.parseInt(text28.getText()), Integer.parseInt(text29.getText())},
+                {Integer.parseInt(text31.getText()), Integer.parseInt(text32.getText()), Integer.parseInt(text33.getText()), Integer.parseInt(text34.getText()), Integer.parseInt(text35.getText()), Integer.parseInt(text36.getText()), Integer.parseInt(text37.getText()), Integer.parseInt(text38.getText()), Integer.parseInt(text39.getText())},
+                {Integer.parseInt(text41.getText()), Integer.parseInt(text42.getText()), Integer.parseInt(text43.getText()), Integer.parseInt(text44.getText()), Integer.parseInt(text45.getText()), Integer.parseInt(text46.getText()), Integer.parseInt(text47.getText()), Integer.parseInt(text48.getText()), Integer.parseInt(text49.getText())},
+                {Integer.parseInt(text51.getText()), Integer.parseInt(text52.getText()), Integer.parseInt(text53.getText()), Integer.parseInt(text54.getText()), Integer.parseInt(text55.getText()), Integer.parseInt(text56.getText()), Integer.parseInt(text57.getText()), Integer.parseInt(text58.getText()), Integer.parseInt(text59.getText())},
+                {Integer.parseInt(text61.getText()), Integer.parseInt(text62.getText()), Integer.parseInt(text63.getText()), Integer.parseInt(text64.getText()), Integer.parseInt(text65.getText()), Integer.parseInt(text66.getText()), Integer.parseInt(text67.getText()), Integer.parseInt(text68.getText()), Integer.parseInt(text69.getText())},
+                {Integer.parseInt(text71.getText()), Integer.parseInt(text72.getText()), Integer.parseInt(text73.getText()), Integer.parseInt(text74.getText()), Integer.parseInt(text75.getText()), Integer.parseInt(text76.getText()), Integer.parseInt(text77.getText()), Integer.parseInt(text78.getText()), Integer.parseInt(text79.getText())},
+                {Integer.parseInt(text81.getText()), Integer.parseInt(text82.getText()), Integer.parseInt(text83.getText()), Integer.parseInt(text84.getText()), Integer.parseInt(text85.getText()), Integer.parseInt(text86.getText()), Integer.parseInt(text87.getText()), Integer.parseInt(text88.getText()), Integer.parseInt(text89.getText())},
+                {Integer.parseInt(text91.getText()), Integer.parseInt(text92.getText()), Integer.parseInt(text93.getText()), Integer.parseInt(text94.getText()), Integer.parseInt(text95.getText()), Integer.parseInt(text96.getText()), Integer.parseInt(text97.getText()), Integer.parseInt(text98.getText()), Integer.parseInt(text99.getText())},};
 
+
+        originalBoard = board;
+
+      
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] != 0) {
+                    
+
+                    
+                    forbiddenMove[i][j] = true;
+
+                    
+                    numberPool.get(j).remove(new Integer(board[i][j]));
+                }
+            }
+        }
+        fillBlanks();
+    }
 
 
 
